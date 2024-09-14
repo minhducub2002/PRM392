bb38154e2d57835c18779f652befa998cb61c66e
minhducub2002 <minhducub2002@gmail.com>
2024-09-14 22:51:51 +0700

    Upload Project FacebookLogin and InstagramLogin

--- Project/InstagramLogin/app/src/main/java/com/example/instagramlogin/MainActivity.java
+++ Project/InstagramLogin/app/src/main/java/com/example/instagramlogin/MainActivity.java
@@ -1,7 +1,6 @@
-package com.example.helloworld;
+package com.example.instagramlogin;
 
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,14 +15,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.btn), (v, insets) -> {
-//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-//            return insets;
-//        });
-
-        Button btn = findViewById(R.id.btn);
-
-
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
     }
 }
