481ab0ed9358fd8f83db5281e1caf38b70712e33
minhducub2002 <minhducub2002@gmail.com>
2024-10-02 23:57:10 +0700

    update Menu in action bar for BTVN_RecyclerView

--- Project/BTVN_RecyclerView/app/src/main/java/com/DucNM/myapplication/MainActivity.java
+++ Project/BTVN_RecyclerView/app/src/main/java/com/DucNM/myapplication/MainActivity.java
@@ -1,12 +1,15 @@
 package com.DucNM.myapplication;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
+import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -55,6 +58,18 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.opt_search) {
+            Toast.makeText(this, "opt_search", Toast.LENGTH_SHORT).show();
+        }
+        if (id == R.id.opt_shopping_cart) {
+            Toast.makeText(this, "opt_shopping_cart", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
